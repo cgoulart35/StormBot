735baba93374f3a8687ecbc644d365b91d865f65
Christopher Goulart <cgoulart35@gmail.com>
2020-10-05 00:25:57 -0400

    Basic Functionality

---
+++ cg-bot/Modules/Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace cg_bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        public async Task Test()
+        {
+            await ReplyAsync("Test Passed.");
+        }
+
+    }
+}
--- cg-bot/Program.cs
+++ cg-bot/Program.cs
@@ -1,21 +1,70 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 
 namespace cg_bot
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+    public class Program
+    { 
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+
+            // If another bot sent the message, then return
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("~", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
         }
     }
 }
